f093dabbccea392c11b4a93f450014ce0e05e946
16569 <osamu_cyber@me.com>
2021-08-22 00:40:41 +0900

    Initial Commit

---
+++ BulletinBoard/BulletinBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletinBoardApp.swift
+//  BulletinBoard
+//
+//  Created by 藤浦　道 on 2021/08/22.
+//
+
+import SwiftUI
+
+@main
+struct BulletinBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BulletinBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BulletinBoard
+//
+//  Created by 藤浦　道 on 2021/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
